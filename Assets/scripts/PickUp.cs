0c95b83a1f537ab4aef5b45719e25f91bcbe448f
Student <uscStudent@none.com>
2016-09-16 23:30:36 -0700

    Add if checks for condition when left & right trigger touch a same collider.

--- Assets/scripts/PickUp.cs
+++ Assets/scripts/PickUp.cs
@@ -26,18 +26,28 @@ public class PickUp : MonoBehaviour {
 	}
 
 	void OnTriggerStay(Collider col){
-		if (fixedJoint == null && device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+		if (fixedJoint == null && device.GetTouch (SteamVR_Controller.ButtonMask.Trigger) && col.GetComponent<FixedJoint>() == null) {
 			Debug.Log ("Get Touched!\n");
 			fixedJoint = col.gameObject.AddComponent<FixedJoint> ();
 			fixedJoint.connectedBody = this.GetComponent<Rigidbody> ();
 		} else if(fixedJoint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger)){
+            Debug.Log("Detected!\n");
 			GameObject go = fixedJoint.gameObject;
 			Rigidbody rigidBody = go.GetComponent<Rigidbody> ();
 			Object.Destroy (fixedJoint);
 			fixedJoint = null;
 			Debug.Log ("Get Touch Up!\n");
 			tossObject (rigidBody);
-		}
+		} else if(fixedJoint != null && !device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GameObject go = fixedJoint.gameObject;
+            Rigidbody rigidBody = go.GetComponent<Rigidbody>();
+            Object.Destroy(fixedJoint);
+            fixedJoint = null;
+            Debug.Log("UnTouched!\n");
+            tossObject(rigidBody);
+            Debug.Log("UnTouched and tossed!\n");
+        }
 	}
 
 	void tossObject(Rigidbody rigidbody){
