8b792e3cea9c55b1f845110fd8d860f0e4640420
Student <uscStudent@none.com>
2016-09-23 17:23:04 -0700

    PortalUsable
    debuged

--- Assets/Scripts/PortalUsable.cs
+++ Assets/Scripts/PortalUsable.cs
@@ -11,8 +11,10 @@ public class PortalUsable : VRTK_InteractableObject
     {
         base.StartUsing(usingObject);
         Debug.Log(this.name + " is used in OnInteractableObjUsed");
+        Debug.Log(usingObject.name+"is using");
         Vector3 newPosition = exit.transform.position;
-        usingObject.transform.position = newPosition;
+        usingObject.transform.parent.position = newPosition;
+        Debug.Log(usingObject.transform.parent.name);
     }
 
     protected override void Start()
