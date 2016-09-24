5a87cd4255c6b425b19cecafb3dddaebb96f4631
Moskey <zhai1990qiang@gmail.com>
2016-09-23 17:07:51 -0700

    PortalDebug
    make portal usable

---
+++ Assets/Scripts/PortalUsable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+
+
+public class PortalUsable : VRTK_InteractableObject
+{
+    public GameObject exit;
+    public override void StartUsing(GameObject usingObject)
+    {
+        base.StartUsing(usingObject);
+        Debug.Log(this.name + " is used in OnInteractableObjUsed");
+        Vector3 newPosition = exit.transform.position;
+        usingObject.transform.position = newPosition;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+    }
+
+}
