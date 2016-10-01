ad1b82c464419b0290b07890cd0d1da51cabf532
Moskey <zhai1990qiang@gmail.com>
2016-10-01 15:50:48 -0700

    ScriptForElecSwitch added
    ScriptForElecSwitch added

--- Assets/Room3/Scripts/Room3_ElectricSwitch.cs
+++ Assets/Room3/Scripts/Room3_ElectricSwitch.cs
@@ -1,15 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using VRTK;
 
-public class Room3_ElectricSwitch : MonoBehaviour {
+public class Room3_ElectricSwitch : VRTK_InteractableObject
+{
+    public GameObject cagePillars;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private bool isOn;
+
+    //initial
+    protected override void Start()
+    {
+        base.Start();
+        isOn = false;
+        //show cagePillars
+        Renderer[] ChildRends = cagePillars.GetComponentsInChildren<Renderer>();
+        foreach (Renderer i in ChildRends) {
+            i.enabled = true;
+        }
+    }
+    //when using
+    public override void StartUsing(GameObject usingObject)
+    {
+        base.StartUsing(usingObject);
+        Debug.Log(this.name + " is used in OnInteractableObjUsed");
+        isOn = !isOn;
+        //change color of ElectricLine tag
+        var lines = GameObject.FindGameObjectsWithTag("ElectricLine");
+        foreach (GameObject i in lines) {
+            Renderer rend = i.GetComponent<Renderer>();
+            rend.material.shader = Shader.Find("Standard");
+            rend.material.color = Color.yellow;
+        }
+        //hide cagePillars
+        Renderer[] ChildRends = cagePillars.GetComponentsInChildren<Renderer>();
+        foreach (Renderer i in ChildRends)
+        {
+            i.enabled = false;
+        }
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        SwitchEffect();
+    }
+
+    //call for the stauts of the switch, return bool;
+    public bool AskStatus() {
+        return isOn;
+    }
+
+    //set the gameobj's transform to show the status of the switch
+    private void SwitchEffect() {
+        if (isOn)
+        {
+            //On status
+            this.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        else
+        {
+            //off status
+            this.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+
+
+
 }
