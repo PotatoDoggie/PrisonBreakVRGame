3f93de265a2de6a514a7aeaa1221592ca67ed472
Moskey <zhai1990qiang@gmail.com>
2016-10-01 16:41:22 -0700

    ElectSwitch fix
    add new lines, and fix the switch

--- Assets/Room3/Scripts/Room3_ElectricSwitch.cs
+++ Assets/Room3/Scripts/Room3_ElectricSwitch.cs
@@ -10,7 +10,7 @@ public class Room3_ElectricSwitch : VRTK_InteractableObject
 
     //initial
     protected override void Start()
-    {
+    { 
         base.Start();
         isOn = false;
         //show cagePillars
@@ -25,47 +25,88 @@ public class Room3_ElectricSwitch : VRTK_InteractableObject
         base.StartUsing(usingObject);
         Debug.Log(this.name + " is used in OnInteractableObjUsed");
         isOn = !isOn;
-        //change color of ElectricLine tag
-        var lines = GameObject.FindGameObjectsWithTag("ElectricLine");
-        foreach (GameObject i in lines) {
-            Renderer rend = i.GetComponent<Renderer>();
-            rend.material.shader = Shader.Find("Standard");
-            rend.material.color = Color.yellow;
-        }
-        //hide cagePillars
-        Renderer[] ChildRends = cagePillars.GetComponentsInChildren<Renderer>();
-        foreach (Renderer i in ChildRends)
-        {
-            i.enabled = false;
-        }
+        LinesEffect();
+        PillarEffect();
+       
     }
 
     protected override void Update()
     {
         base.Update();
         SwitchEffect();
+        LinesEffect();
     }
 
     //call for the stauts of the switch, return bool;
     public bool AskStatus() {
         return isOn;
     }
-
+    //
+    private void PillarEffect()
+    {
+        if (isOn)
+        {
+            //hide cagePillars
+            Renderer[] ChildRends = cagePillars.GetComponentsInChildren<Renderer>();
+            foreach (Renderer i in ChildRends)
+            {
+                i.enabled = false;
+            }
+        }
+        else
+        {
+            //hide cagePillars
+            Renderer[] ChildRends = cagePillars.GetComponentsInChildren<Renderer>();
+            foreach (Renderer i in ChildRends)
+            {
+                i.enabled = true;
+            }
+        }
+        
+    }
     //set the gameobj's transform to show the status of the switch
-    private void SwitchEffect() {
+    private void SwitchEffect()
+    {
         if (isOn)
         {
             //On status
-            this.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            this.transform.Rotate(45f, 0f, 0f);
         }
         else
         {
             //off status
-            this.transform.localScale = new Vector3(1f, 1f, 1f);
+            this.transform.Rotate(-45f, 0f, 0f);
         }
+    }
+    //set the electric lines color
+    private void LinesEffect()
+    {
+        if (isOn)
+        {
+            //change color of ElectricLine tag
+            var lines = GameObject.FindGameObjectsWithTag("ElectricLine");
+            foreach (GameObject i in lines)
+            {
+                Renderer rend = i.GetComponent<Renderer>();
+                rend.material.shader = Shader.Find("Standard");
+                rend.material.color = Color.yellow;
+            }
+        }
+        else {
+            //change color of ElectricLine tag
+            var lines = GameObject.FindGameObjectsWithTag("ElectricLine");
+            foreach (GameObject i in lines)
+            {
+                Renderer rend = i.GetComponent<Renderer>();
+                rend.material.shader = Shader.Find("Standard");
+                rend.material.color = Color.white;
+            }
+        }
+
+    }
     }
 
 
 
 
-}
+
