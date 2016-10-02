334eddde90f5128df3ee2303d932c1bf3758df35
Moskey <zhai1990qiang@gmail.com>
2016-10-01 17:59:18 -0700

    AddBroken&RepairedLines
    add broken and repairedElectric lines, add Tape tag, when tape
    colliderenter, repair

---
+++ Assets/BrokenLine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrokenLine : MonoBehaviour {
+    public GameObject repairedLine;
+
+	// Use this for initialization
+	void Start () {
+        repairedLine.SetActive(false);
+        Renderer rend = this.GetComponent<Renderer>();
+        rend.enabled = true;
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Tape"))
+        {
+            repairedLine.SetActive(true);
+            Renderer rend = this.GetComponent<Renderer>();
+            rend.enabled = false;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
