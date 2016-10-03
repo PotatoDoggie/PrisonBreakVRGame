1c9b40b12de769b76f974b0a3c473dea31bd1b72
wuyunxin <wuyunxin1993@outlook.com>
2016-10-03 01:24:56 -0700

    Add scripts to screw driver and key

---
+++ Assets/Scripts/ScrewDriverController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrewDriverController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("ScrewHead")) {
+			other.gameObject.SetActive (false);
+			count++;
+			if (count >= 4) {
+				GameObject electricPanelDoor = GameObject.Find ("ElectricPanelDoor");
+				float f = 1.5f;
+				electricPanelDoor.transform.position = new Vector3(electricPanelDoor.transform.position.x, 
+					electricPanelDoor.transform.position.y - f, 
+					electricPanelDoor.transform.position.z);
+			}
+		}
+	}
+}
