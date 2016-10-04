e01741f7d13f6eb9e180f4248bed2180222f725a
wuyunxin <wuyunxin1993@outlook.com>
2016-10-04 15:09:32 -0700

    add script for tape

---
+++ Assets/Scripts/TapeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapeController : MonoBehaviour {
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		GameObject brokenwire = GameObject.Find ("Broken wire");
+		if (other.Equals(brokenwire)) {
+			other.gameObject.SetActive (false);
+			GameObject healthywire = GameObject.Find ("healthy wire");
+			MeshRenderer mr = healthywire.GetComponent<MeshRenderer>();
+			mr.enabled = true;
+		}
+	}
+}
