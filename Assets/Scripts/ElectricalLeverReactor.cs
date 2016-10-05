61b2516deea59c4fe2b68c66471f87bb3caf8760
Student <uscStudent@none.com>
2016-10-04 19:27:52 -0700

    Add lever and script for Electric lever. Add script for key controll

---
+++ Assets/Scripts/ElectricalLeverReactor.cs
@@ -0,0 +1,36 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+
+    public class    ElectricalLeverReactor : MonoBehaviour
+    {
+        public Renderer eLight;
+        public bool ElectricOn;
+
+        public TextMesh go;
+
+        private void Start()
+        {
+            GetComponent<VRTK_Control>().defaultEvents.OnValueChanged.AddListener(HandleChange);
+            HandleChange(GetComponent<VRTK_Control>().GetValue(), GetComponent<VRTK_Control>().GetNormalizedValue());
+        }
+
+        private void HandleChange(float value, float normalizedValue)
+        {
+            if (normalizedValue > 60)
+            {
+                go.text = "Electric OFF!";
+                ElectricOn = false;
+                /*key.isGrabbable = true;
+                key.holdButtonToGrab = false;
+                key.grabAttachMechanic = VRTK_InteractableObject.GrabAttachType.Child_Of_Controller;*/
+                eLight.material.color = Color.red;
+            }
+            else {
+                go.text = "Electric ON!";
+                ElectricOn = true;
+                eLight.material.color = Color.green;
+            }
+        }
+    }
+}
--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -1,21 +1,36 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿namespace VRTK
+{
+    using UnityEngine;
+    using System.Collections;
 
-public class KeyController : MonoBehaviour {
+    public class KeyController : MonoBehaviour
+    {
 
-	private Rigidbody rb;
+        private Rigidbody rb;
+        private ElectricalLeverReactor electricOn;
+        private bool wireFixed;
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody> ();
-	}
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            electricOn = GetComponent<ElectricalLeverReactor>();
+        }
 
-	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag ("KeyHole")) {
-			GameObject doorLeft = GameObject.FindGameObjectWithTag ("DoorLeft");
-			GameObject doorRight = GameObject.FindGameObjectWithTag ("DoorRight");
-			doorLeft.transform.Rotate (new Vector3(0, 90, 0));
-			doorRight.transform.Rotate (new Vector3 (0, -90, 0));
-		}
-	}
+        void Update()
+        {
+
+        }
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("KeyHole"))
+            {
+                GameObject doorLeft = GameObject.FindGameObjectWithTag("DoorLeft");
+                GameObject doorRight = GameObject.FindGameObjectWithTag("DoorRight");
+                doorLeft.transform.Rotate(new Vector3(0, 90, 0));
+                doorRight.transform.Rotate(new Vector3(0, -90, 0));
+            }
+        }
+    }
 }
+
