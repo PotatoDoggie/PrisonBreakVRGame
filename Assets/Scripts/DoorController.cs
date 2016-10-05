88fee3efb486cf4549ad86920824ff9bd761770c
wuyunxin <wuyunxin1993@outlook.com>
2016-10-05 01:55:39 -0700

    Add script for keypad & outdoor

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+	public bool doorOpen = false;
+}
--- Assets/Scripts/KeypadController.cs
+++ Assets/Scripts/KeypadController.cs
@@ -2,92 +2,108 @@
 using VRTK;
 using System.Collections;
 
-public class KeypadController : MonoBehaviour
+public class KeypadController : VRTK_InteractableObject
 {
-
     private string curPassword = "2628";
     private string input;
-    private bool doorOpen = false;
-    private bool keypadScreen;
+    private bool doorOpen;
+    //private bool keypadScreen;
  
     //public Transform doorHinge;
-    public GameObject screen;
-    public GameObject door;
-    public ElightController eLight;
+    private GameObject screen;
+    private GameObject door;
+    private ElightController eLight;
+	private TextMesh tm;
 
-    void OnTriggerEnter(Collider other)
+	protected override void Start() {
+		base.Start ();
+		screen = GameObject.Find ("Screen");
+		door = GameObject.Find ("PrisonCellDoorWayDoor");
+		eLight = GameObject.Find ("ElecticLight").GetComponent<ElightController> ();
+		tm = screen.GetComponent<TextMesh>();
+	}
+
+	public override void StartUsing(GameObject usingObject)
+	{
+		base.StartUsing(usingObject);
+		pressButtons(usingObject.name);
+	}
+
+	void pressButtons(string name)
     {
-        TextMesh tm = screen.GetComponent<TextMesh>();
+		input = tm.text;
+		doorOpen = door.GetComponent<DoorController> ().doorOpen;
         if (eLight.powerOn) {
             if (!doorOpen)
             {
-                if (other.name.Equals("Cube_0"))
+                if (name.Equals("Cube_0"))
                 {
                     input += "0";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_1"))
+                else if (name.Equals("Cube_1"))
                 {
                     input += "1";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_2"))
+                else if (name.Equals("Cube_2"))
                 {
                     input += "2";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_3"))
+                else if (name.Equals("Cube_3"))
                 {
                     input += "3";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_4"))
+                else if (name.Equals("Cube_4"))
                 {
                     input += "4";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_5"))
+                else if (name.Equals("Cube_5"))
                 {
                     input += "5";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_6"))
+                else if (name.Equals("Cube_6"))
                 {
                     input += "6";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_7"))
+                else if (name.Equals("Cube_7"))
                 {
                     input += "7";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_8"))
+                else if (name.Equals("Cube_8"))
                 {
                     input += "8";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_9"))
+                else if (name.Equals("Cube_9"))
                 {
                     input += "9";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_cancel"))
+                else if (name.Equals("Cube_cancel"))
                 {
                     input = "";
                     tm.text = input;
                 }
-                else if (other.name.Equals("Cube_enter"))
+                else if (name.Equals("Cube_enter"))
                 {
                     if (input.Equals(curPassword))
                     {
                         doorOpen = true;
+						door.GetComponent<DoorController> ().doorOpen = doorOpen;
                         input = "";
-                        tm.text = "Congraduation!";
+                        tm.text = "Congratuations!";
                     }
                     else
                     {
                         input = "";
-                        tm.text = "Password Wrong! ";
+                        tm.text = "Password Wrong!";
                     }
                 }
                 if (doorOpen)
@@ -96,6 +112,6 @@ public class KeypadController : MonoBehaviour
                 }
             }
         }
-    }
 
+    }
 }
