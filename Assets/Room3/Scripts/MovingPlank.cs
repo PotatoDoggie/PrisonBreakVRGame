a5e18adeb04259c0e00c7c0509167e21ce6a2a7f
brandonpeng <TonyPeng@Pengs-MacBook-Air.local>
2016-10-08 02:58:52 -0700

    Room3_2

---
+++ Assets/Room3/Scripts/MovingPlank.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlank : MonoBehaviour 
+{
+	
+	void Update() 
+	{
+		transform.position = new Vector3(Mathf.PingPong(Time.time, 2.5f), transform.position.y, transform.position.z);
+	}
+}
