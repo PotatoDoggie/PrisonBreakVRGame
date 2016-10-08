15aac088388c2c2c888f6ecb81080d00ea927b62
Moskey <zhai1990qiang@gmail.com>
2016-10-08 12:12:03 -0700

    DestriableBallon&MovingPlankwithfriction
    use rb.movepostion to includes friciton for moving plank, add scripts
    for ballon prefab

--- Assets/Room3/Scripts/MovingPlank.cs
+++ Assets/Room3/Scripts/MovingPlank.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class MovingPlank : MonoBehaviour 
 {
-	
-	void Update() 
-	{
-		transform.position = new Vector3(Mathf.PingPong(Time.time, 2.5f), transform.position.y, transform.position.z);
+	private float speed = 2.0f;
+    void Update() 
+	{   //moveposition includes fiction
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.MovePosition (new Vector3(Mathf.PingPong(Time.time, 2.5f), transform.position.y, transform.position.z));
 	}
+    /*
+     void FixedUpdate() {
+         Rigidbody rigidbody = GetComponent<Rigidbody>();
+         rigidbody.position -= transform.forward * speed * Time.deltaTime;
+         rigidbody.MovePosition(rigidbody.position + transform.forward * speed * Time.deltaTime);
+     }
+      */
 }
