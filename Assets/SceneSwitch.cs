37687c9c5185b2575e5104641b3a246707693015
Student <uscStudent@none.com>
2016-10-12 13:46:41 -0700

    Won't crash if build first. (Unity is garbage)

---
+++ Assets/SceneSwitch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneSwitch : MonoBehaviour {
+    public int destination = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("e"))
+        {
+            SceneManager.LoadScene(destination);
+        }
+
+
+    }
+}
