c9292d3a976376492bc117694bd70013ce6d89fa
Student <uscStudent@none.com>
2016-10-14 00:08:39 -0700

    Change the key shade, keypad password, keypad cancle input

--- Assets/Scripts/ElectricalLeverReactor.cs
+++ Assets/Scripts/ElectricalLeverReactor.cs
@@ -7,8 +7,6 @@
         public Renderer eLight;
         public bool ElectricOn;
 
-        public TextMesh go;
-
         private void Start()
         {
             GetComponent<VRTK_Control>().defaultEvents.OnValueChanged.AddListener(HandleChange);
@@ -20,14 +18,12 @@
 			GameObject[] objs = GameObject.FindGameObjectsWithTag ("Light");
             if (normalizedValue > 60)
             {
-                go.text = "Electric OFF!";
                 ElectricOn = false;
 				for (int i = 0; i < objs.Length; i++) {
 					objs [i].GetComponent<Light> ().enabled = false;
 				}
             }
             else {
-                go.text = "Electric ON!";
                 ElectricOn = true;
 				for (int i = 0; i < objs.Length; i++) {
 					objs [i].GetComponent<Light> ().enabled = true;
