a4c4497828d8b41c198003738be8e60e42ad5dc4
Student <uscStudent@none.com>
2016-11-06 23:27:01 -0800

    fix bugs for tutorial room. Qus:1)rotation angle (second time when pushing the handle)  2) the angle of box_top. 3) Floor: blur.

--- Assets/TutorialRoom/Scripts/DoorHandle.cs
+++ Assets/TutorialRoom/Scripts/DoorHandle.cs
@@ -3,17 +3,24 @@ using System.Collections;
 using VRTK;
 
 public class DoorHandle : VRTK_InteractableObject {
+	private SwitchLever switchLever;
 	private bool leftDoorOpen;
 	private bool rightDoorOpen;
 	private GameObject leftDoor;
 	private GameObject rightDoor;
+	private Vector3 oldPositionLeft;
+	private Vector3 oldPositionRight;
 
 	protected override void Start() {
 		base.Start ();
+		//prisonDoorOpen = switchLever.prisondoorOpen;
+		switchLever = GameObject.Find("Lever").GetComponent<SwitchLever>();
 		leftDoorOpen = false;
 		rightDoorOpen = false;
 		leftDoor = GameObject.Find ("LeftDoor");
 		rightDoor = GameObject.Find ("RightDoor");
+		oldPositionLeft = leftDoor.transform.rotation.eulerAngles;
+		oldPositionRight = rightDoor.transform.rotation.eulerAngles;
 	}
 		
 	public override void StartUsing(GameObject usingObject)
@@ -23,25 +30,26 @@ public class DoorHandle : VRTK_InteractableObject {
 	}
 
 	public void openDoor(GameObject obj) {
-		if (obj.name.Equals ("leftDoorHandle")) {
-			if (leftDoorOpen == false) {
-				leftDoorOpen = true;
-				leftDoor.transform.Rotate (new Vector3 (0, -90, 0));
-			} else {
-				leftDoorOpen = false;
-				leftDoor.transform.Rotate (new Vector3 (0, 0, 0));
-			}
+		if (switchLever.prisondoorOpen) {
+			if (obj.name.Equals ("leftDoorHandle")) {
+				if (leftDoorOpen == false) {
+					leftDoorOpen = true;
+					leftDoor.transform.Rotate (new Vector3 (0, -90, 0));
+				} else {
+					leftDoorOpen = false;
+					leftDoor.transform.Rotate (oldPositionLeft);
+				}
 
-		}
-		if (obj.name.Equals ("rightDoorHandle")) {
-			if (rightDoorOpen == false) {
-				rightDoorOpen = true;
-				rightDoor.transform.Rotate (new Vector3 (0, 90, 0));
-			} else {
-				rightDoorOpen = false;
-				rightDoor.transform.Rotate (new Vector3(0, 0, 0));
 			}
-
+			if (obj.name.Equals ("rightDoorHandle")) {
+				if (rightDoorOpen == false) {
+					rightDoorOpen = true;
+					rightDoor.transform.Rotate (new Vector3 (0, 90, 0));
+				} else {
+					rightDoorOpen = false;
+					rightDoor.transform.Rotate (oldPositionRight);
+				}
+			}
 		}
 	}
 }
--- Assets/TutorialRoom/Scripts/SwitchLever.cs
+++ Assets/TutorialRoom/Scripts/SwitchLever.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 using VRTK;
 public class SwitchLever : MonoBehaviour {
-	private bool powerOn;
+	public bool prisondoorOpen;
 	private Vector3 oldPosition;
 	private GameObject obj;
 
 	// Use this for initialization
 	void Start () {
+		prisondoorOpen = false;
 		obj = GameObject.Find ("PrisonGateSingleDoor");
+		//oldPosition = new Vector3(0.25f, 0, -0.025f);
 		oldPosition = obj.transform.position;
+		Debug.Log (oldPosition.x + " " + oldPosition.y + " " + oldPosition.z);
 		GetComponent<VRTK_Control>().defaultEvents.OnValueChanged.AddListener(HandleChange);
 		HandleChange(GetComponent<VRTK_Control>().GetValue(), GetComponent<VRTK_Control>().GetNormalizedValue());
 	}
@@ -18,12 +21,12 @@ public class SwitchLever : MonoBehaviour {
 	{
 		if (normalizedValue > 60)
 		{
-			powerOn = false;
-			obj.transform.position = oldPosition;
+			prisondoorOpen = true;
+			obj.transform.position = new Vector3 (oldPosition.x, oldPosition.y + 2.0f, oldPosition.z);
 		}
 		else {
-			powerOn = true;
-			obj.transform.position = new Vector3 (oldPosition.x, oldPosition.y + 3, oldPosition.z);
+			prisondoorOpen = false;
+			obj.transform.position = oldPosition;
 		}
 	}
 }
