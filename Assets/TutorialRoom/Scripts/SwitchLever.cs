a1e48dd6dacf420c9a6d7d1277b85f3eb2b8d736
wuyunxin <wuyunxin1993@outlook.com>
2016-11-06 21:12:48 -0800

    Add script for switch.

---
+++ Assets/TutorialRoom/Scripts/SwitchLever.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+public class SwitchLever : MonoBehaviour {
+	private bool powerOn;
+	private Vector3 oldPosition;
+	private GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		obj = GameObject.Find ("PrisonGateSingleDoor");
+		oldPosition = obj.transform.position;
+		GetComponent<VRTK_Control>().defaultEvents.OnValueChanged.AddListener(HandleChange);
+		HandleChange(GetComponent<VRTK_Control>().GetValue(), GetComponent<VRTK_Control>().GetNormalizedValue());
+	}
+	
+	private void HandleChange(float value, float normalizedValue)
+	{
+		if (normalizedValue > 60)
+		{
+			powerOn = false;
+			obj.transform.position = oldPosition;
+		}
+		else {
+			powerOn = true;
+			obj.transform.position = new Vector3 (oldPosition.x, oldPosition.y + 3, oldPosition.z);
+		}
+	}
+}
+
