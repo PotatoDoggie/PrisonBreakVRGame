7ac588527c449fa3797d55a601f9e618b0c384e5
wuyunxin <wuyunxin1993@outlook.com>
2016-11-06 20:44:58 -0800

    Add script for key.

---
+++ Assets/TutorialRoom/Scripts/Key.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Key_Hole"))
+		{
+			GameObject boxTop = GameObject.Find ("box_top");
+			boxTop.transform.Rotate (new Vector3(-45, 0, 0));
+		}
+	}
+}
