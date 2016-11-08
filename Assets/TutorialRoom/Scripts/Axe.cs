87f9ff6fa7cc9da194db7087de982b3234cf8bec
wuyunxin <wuyunxin1993@outlook.com>
2016-11-07 21:26:26 -0800

    Add script to axe to make it open the door.

---
+++ Assets/TutorialRoom/Scripts/Axe.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Axe : MonoBehaviour {
+	private int collisionTimes = 3;
+	GameObject door;
+	GameObject doorHandle;
+	// Use this for initialization
+	void Start () {
+		door = GameObject.Find ("Door");
+		doorHandle = GameObject.Find ("DoorHandle");
+	}
+	
+	void onTriggerEnter(Collider other) {
+		if (other.gameObject.Equals (door) || other.gameObject.Equals (doorHandle)) {
+			collisionTimes--;
+			if (collisionTimes == 0) {
+				door.transform.Rotate (new Vector3 (0, 45, 0));
+			}
+		}
+	}
+}
